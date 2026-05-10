254a7467bdeb6156a9573769453ee1d8412699fa
filipmoco0 <113348795+filipmoco0@users.noreply.github.com>
2026-05-10 19:48:21 +0200

    Update Pravokutnik.cs

--- zadatak3/Pravokutnik.cs
+++ zadatak3/Pravokutnik.cs
@@ -1,45 +1,56 @@
 using System;
-using System.Collections.Generic;
 
-class Pravokutnik : GeometrijskiLik
+namespace Zadatak3;
+
+public class Pravokutnik : IGeometrijskiLik, IUsporediPovrsinu<IGeometrijskiLik>
 {
-    public Pravokutnik(float a, float b2, string b) : base(b)
-    {
-        _a = a;
-        _b = b2;
-    }
-    
-    ~Pravokutnik()
-    {
-        Console.WriteLine("Unisten Pravokutnik!");
-    }
-    
     protected float _a, _b;
+
+    public string Boja { get; set; }
 
     public Pravokutnik()
     {
         _a = 0;
         _b = 0;
+        Boja = "Zelena";
     }
 
     public Pravokutnik(float a, float b)
     {
         _a = a;
         _b = b;
+        Boja = "Zelena";
     }
 
-    public override void Tip()
+    public Pravokutnik(string bo, float a, float b)
+    {
+        Boja = bo;
+        _a = a;
+        _b = b;
+    }
+
+    public void Tip()
     {
         Console.WriteLine("Pravokutnik sa stranicama duljine " + _a + " i " + _b);
     }
 
-    public override float Povrsina()
+    public float Povrsina()
     {
         return _a * _b;
     }
 
-    public override float Opseg()
+    public float Opseg()
     {
         return 2 * _a + 2 * _b;
+    }
+
+    public int UsporediPovrsinu(IGeometrijskiLik tmp)
+    {
+        if (Povrsina() == tmp.Povrsina())
+            return 0;
+        else if (Povrsina() > tmp.Povrsina())
+            return 1;
+        else
+            return 2;
     }
 }
