633f5dc72a0866368e9f3fe5319a6815e1c15bbb
filipmoco0 <113348795+filipmoco0@users.noreply.github.com>
2026-05-17 17:27:48 +0200

    Delete zadatak3 directory

--- zadatak3/IGeometrijskiLik.cs
+++
@@ -1,9 +0,0 @@
-namespace Zadatak3;
-
-public interface IGeometrijskiLik
-{
-    string Boja { get; set; }
-    void Tip();
-    float Opseg();
-    float Povrsina();
-}
--- zadatak3/IUsporediPovrsinu.cs
+++
@@ -1,6 +0,0 @@
-namespace Zadatak3;
-
-public interface IUsporediPovrsinu<T>
-{
-    int UsporediPovrsinu(T tmp);
-}
